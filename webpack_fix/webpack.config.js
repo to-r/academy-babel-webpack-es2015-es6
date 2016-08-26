616bda358d17d5c3d7e244a94a79088a29a9ad4b
KazumaNishihata <nishihata@to-r.net>
2016-08-26 22:41:36 +0900

    webpackからwebpack_dirに変更、xxxx_fix追加

---
+++ webpack_fix/webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  entry: './src/script.js',
+  output: {
+    path:"./dist",
+    filename: 'script.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+         loader: "babel-loader"
+      }
+    ]
+  }
+};
