6af3a37ac0690aac5ea7fc5ec9e2e48c59b5e0de
KazumaNishihata <nishihata@to-r.net>
2017-11-16 19:24:11 +0900

    アップデート

--- fix/webpack.config.js
+++ fix/webpack.config.js
@@ -1,16 +1,18 @@
+var path = require('path');
+
 module.exports = {
-	entry: './src/script.js',
-	output: {
-		path:"./dist",
-		filename: 'script.js'
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: "babel-loader"
-			}
-		]
-	}
+    entry: "./src/script.js",
+    output: {
+        path:path.resolve(__dirname, "dist"),
+        filename: "script.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    }
 };
